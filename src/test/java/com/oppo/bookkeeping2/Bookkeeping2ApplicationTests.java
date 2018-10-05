7961b956b1817633f76b94c9f7e12a9a1d6e0c25
jiechen12345 <cheners123@gmail.com>
2018-10-05 22:18:37 +0800

    基本表單完成，做新增

---
+++ src/test/java/com/oppo/bookkeeping2/Bookkeeping2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oppo.bookkeeping2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bookkeeping2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
