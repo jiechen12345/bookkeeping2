db1f3b074280714c94ec0c445678913ab0517cde
jiechen12345 <cheners123@gmail.com>
2018-10-12 15:42:03 +0800

    CascadeType 修正

--- src/main/java/com/oppo/api/MemberApi.java
+++ src/main/java/com/oppo/api/MemberApi.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -28,33 +29,37 @@ public class MemberApi {
     private DepartmentDao departmentDao;
     @Autowired
     private MemberService memberService;
+    Integer [] pageSizeOption= {5, 10, 15, 20};
 
-/*
-    //查詢會員列表
-    @GetMapping("/members")
-    public String list( Model model) {
-        int pageSize=3;
-        MemberPage memberPage = memberService.getAllForm(1,pageSize);
-        model.addAttribute("members", memberPage.getContents());
-        model.addAttribute("indexPage", memberPage.getCurrentPage());
-        model.addAttribute("totalPages", memberPage.getTotalPages());
-        model.addAttribute("count", memberPage.getCount());
-        model.addAttribute("pageSize", pageSize);
-        return "member/list";
+//    List<Integer> pageSizeOption=new ArrayList<Integer>()
 
-    }
-*/
+    /*
+        //查詢會員列表
+        @GetMapping("/members")
+        public String list( Model model) {
+            int pageSize=3;
+            MemberPage memberPage = memberService.getAllForm(1,pageSize);
+            model.addAttribute("members", memberPage.getContents());
+            model.addAttribute("indexPage", memberPage.getCurrentPage());
+            model.addAttribute("totalPages", memberPage.getTotalPages());
+            model.addAttribute("count", memberPage.getCount());
+            model.addAttribute("pageSize", pageSize);
+            return "member/list";
+
+        }
+    */
     //查詢分頁會員列表(暫無條件)
     @GetMapping("/members/{page}")
     public String getAllForms(@PathVariable(required = false) Integer page, Model model) {
-        int pageSize=5;
-        MemberPage memberPage = memberService.getAllForm(page,pageSize);
+        int pageSize = 5;
+        MemberPage memberPage = memberService.getAllForm(page, pageSize);
         //List<MemberDto> memberDtoList = memberService.findAll();
         model.addAttribute("members", memberPage.getContents());
         model.addAttribute("indexPage", memberPage.getCurrentPage());
         model.addAttribute("totalPages", memberPage.getTotalPages());
         model.addAttribute("pageSize", pageSize);
         model.addAttribute("count", memberPage.getCount());
+        model.addAttribute("pageSizeOption", pageSizeOption);
         return "member/list";
 
     }
@@ -62,14 +67,15 @@ public class MemberApi {
     //查詢分頁會員列表及修改pageSize
     @GetMapping("/members")
     public String changePageSize(@RequestParam(required = false, defaultValue = "1") Integer page,
-                                 @RequestParam(required = false, defaultValue = "5")Integer pageSize ,Model model) {
-        MemberPage memberPage = memberService.getAllForm(page,pageSize);
+                                 @RequestParam(required = false, defaultValue = "5") Integer pageSize, Model model) {
+        MemberPage memberPage = memberService.getAllForm(page, pageSize);
         //List<MemberDto> memberDtoList = memberService.findAll();
         model.addAttribute("members", memberPage.getContents());
         model.addAttribute("indexPage", memberPage.getCurrentPage());
         model.addAttribute("totalPages", memberPage.getTotalPages());
         model.addAttribute("pageSize", pageSize);
         model.addAttribute("count", memberPage.getCount());
+        model.addAttribute("pageSizeOption", pageSizeOption);
         return "member/list";
     }
 
